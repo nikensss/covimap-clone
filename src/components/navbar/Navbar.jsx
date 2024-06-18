ac53225cceea6f28999797b43f25eed83afab6f1
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-06-18 15:06:34 +0200

    instalar tailwind

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,11 +10,15 @@ const Navbar = () => {
               <li>
                 C:\Users\Esther_\Documents\FactoriaF5-proyectos\Covid_Tracker\CovidTracker\
                 <a href="">
-                  <img src="../public/assets/images/coronavirus.png" alt="" />
+                  <img
+                    src="../public/assets/images/coronavirus.png"
+                    alt="logo covid"
+                  />
                 </a>
               </li>
               <li>
                 <a href=""></a>
+                <span className="bg-blue-500 text-white p-4"></span>
               </li>
               <li>
                 <a href=""></a>
