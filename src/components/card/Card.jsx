e6fecc06cd308f1c83df4a43957b54b23ef4d77a
helopgom <helena.lopez.opos@gmail.com>
2024-06-18 16:17:03 +0200

    Estructura card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div classN>
-      
+    <div className="bg-gray-50 ">
+      <div className='info'>
+        <div className="icon">
+          <img scr={"public\assets\icons\covid-blue.svg"} alt="iconCovid"/>
+          <div className="info">
+            <h5>Total Cases</h5>
+            <h3 className="block ">86503057</h3>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
