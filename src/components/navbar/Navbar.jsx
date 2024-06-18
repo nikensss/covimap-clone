518c630fffccaad12d6817dd2461714cc0c3fac7
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-06-18 14:26:29 +0200

    creada estructura navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
-      
+      <nav>
+        <div>
+          <div>
+            <ul>
+              <li>
+                C:\Users\Esther_\Documents\FactoriaF5-proyectos\Covid_Tracker\CovidTracker\
+                <a href="">
+                  <img src="../public/assets/images/coronavirus.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
