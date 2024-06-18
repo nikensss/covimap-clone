88846760f3b2c4693ae081e5dd75558756999982
indiakka <78439130+indiakka@user.noreply.github.com>
2024-06-18 14:41:13 +0200

    corregir rutas

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout.jsx";
-import Home from '../pages/home'
-import Tracker1 from "../pages/tracker1";
-import Tracker2 from "../pages/tracker2";
-import Tracker3 from "../pages/tracker3";
-import Tracker4 from "../pages/tracker4";
-import Tracker5 from "../pages/tracker5";
-import Tracker6 from "../pages/tracker6";
+import Home from '../pages/home/Home.jsx'
+import Tracker1 from "../pages/tracker1/Tracker1.jsx";
+import Tracker2 from "../pages/tracker2/Tracker2.jsx";
+import Tracker3 from "../pages/tracker3/Tracker3.jsx";
+import Tracker4 from "../pages/tracker4/Tracker4.jsx";
+import Tracker5 from "../pages/tracker5/Tracker5.jsx";
+import Tracker6 from "../pages/tracker6/Tracker6.jsx";
 
 export const router = createBrowserRouter([
   {
