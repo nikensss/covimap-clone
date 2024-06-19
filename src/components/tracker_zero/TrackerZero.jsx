8f8831cb8a63878cd2f15418e0cb6014d9bada6f
Deiximar Virriel <deiximar@gmail.com>
2024-06-19 19:20:45 +0200

    Agregar animacion a los componentes de traker 0

--- src/components/tracker_zero/TrackerZero.jsx
+++ src/components/tracker_zero/TrackerZero.jsx
@@ -4,7 +4,7 @@ import CountryTop from "./CountryTop";
 const TrackerZero = () => {
   return (
     <div
-      className="w-96 px-2.5 py-5 fixed left-24 border-r border-solid border-strongGray z-1 h-screen 
+      className="w-96 px-3 py-5 fixed left-24 border-r border-solid border-strongGray z-1 h-screen 
       scrollBar overflow-hidden focus:overflow-y-scroll hover:overflow-y-scroll active:overflow-y-scroll scroll-smooth"
     >
       <GeneralInformation />
