961ab58de542f69b50962758671d4dba2fcb898b
Deiximar Virriel <deiximar@gmail.com>
2024-06-19 17:36:38 +0200

    migrando estilos de css a tailwind

--- src/components/tracker_zero/GeneralInformation.jsx
+++ src/components/tracker_zero/GeneralInformation.jsx
@@ -1,26 +1,50 @@
 const GeneralInformation = () => {
   return (
     <>
-      <h2 className="title">COVID-19 Tracker</h2>
-      <ul className="caseDetailsList">
-        <li className="textLeft">
-          <h6>Total Case</h6>
-          <p>581511314</p>
-          <b>+607496</b>
+      <h2 className="font-bold text-darkBlue text-2xl my-2 leading-tight">
+        COVID-19 Tracker
+      </h2>
+      <ul className="list-none">
+        <li className="bg-chiffon p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent ">
+          <h6 className="text-base font-semibold text-navyBlue inline-block">
+            Total Case
+          </h6>
+          <p className="text-2xl text-steel font-semibold leading-6 float-right">
+            581511314
+          </p>
+          <b className="float-right text-xs text-steel bg-white p-1 mr-2 rounded tracking-wide">
+            +607496
+          </b>
         </li>
-        <li className="textLeft">
-          <h6>Active Case</h6>
-          <p>581511314</p>
+        <li className="bg-chiffon p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent ">
+          <h6 className="text-base font-semibold text-navyBlue inline-block">
+            Active Case
+          </h6>
+          <p className="text-2xl text-steel font-semibold leading-6 float-right">
+            581511314
+          </p>
         </li>
-        <li className="textLeft">
-          <h6>Recovered Case</h6>
-          <p>581511314</p>
-          <b>+0</b>
+        <li className="bg-chiffon p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent ">
+          <h6 className="text-base font-semibold text-navyBlue inline-block">
+            Recovered Case
+          </h6>
+          <p className="text-2xl text-steel font-semibold leading-6 float-right">
+            581511314
+          </p>
+          <b className="float-right text-xs text-steel bg-white p-1 mr-2 rounded tracking-wide">
+            +0
+          </b>
         </li>
-        <li className="textLeft">
-          <h6>Deaths Case</h6>
-          <p>581511314</p>
-          <b>+0</b>
+        <li className="bg-chiffon p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent ">
+          <h6 className="text-base font-semibold text-navyBlue inline-block">
+            Deaths Case
+          </h6>
+          <p className="text-2xl text-steel font-semibold leading-6 float-right">
+            581511314
+          </p>
+          <b className="float-right text-xs text-steel bg-white p-1 mr-2 rounded tracking-wide">
+            +0
+          </b>
         </li>
       </ul>
     </>
